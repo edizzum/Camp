957e4ca12bae454f46ee823737a5dacf827f262b
Ediz Züm <ediz.zum@hotmail.com>
2021-01-21 00:20:45 +0300

    Starter Pack

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Ediz";
+            int yas = 20;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C#"; //Burada tırnak içinde yazılmasının nedeni alttaki class ta string tanımladık
+            kurs1.Egitmeni = "Ediz Züm";
+            kurs1.IzlenmeOrani = 100;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Python";
+            kurs2.Egitmeni = "Barış Gül";
+            kurs2.IzlenmeOrani = 10;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Java";
+            kurs3.Egitmeni = "Güney Taran";
+            kurs3.IzlenmeOrani = 56;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.KursAdi = "PHP";
+            kurs4.Egitmeni = "Umut Mırrıkoğlu";
+            kurs4.IzlenmeOrani = 50;
+
+            //Console.WriteLine(kurs1.Egitmeni + " " + kurs2.IzlenmeOrani);
+            Kurs[] Kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+            Console.WriteLine("Hangi Eğitmen kaç izleniyor; ");
+            foreach (var kurs in Kurslar)
+            {
+                Console.WriteLine(kurs.Egitmeni + ":" + kurs.IzlenmeOrani);
+            }
+        }
+    }
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmeni { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ Donguler/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Kurs1 = "Yazilim gelistirici yetistirme kampi";
+            string Kurs2 = "Programlamaya baslangic icin temel kurs";
+            string Kurs3 = "Java";
+            string Kurs4 = "Python";
+
+            string[] kurslar = new string[] { "Yazilim gelistirici yetistirme kampi" ,
+                "Programlamaya baslangic icin temel kurs",
+                "Java",
+                "Python"};
+
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
