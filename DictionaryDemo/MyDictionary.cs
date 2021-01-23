21d1e3038945917602962bd37a01ae062c563c70
Ediz Züm <ediz.zum@hotmail.com>
2021-01-23 20:51:10 +0300

    Example of how dictionaries work and their features.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] newDictKey;
+        T1[] tempDictKey;
+        T2[] newDictValue;
+        T2[] tempDictValue;
+
+        public MyDictionary()
+        {
+            newDictKey = new T1[1];
+            newDictValue = new T2[1];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            tempDictKey = newDictKey;
+            newDictKey = new T1[newDictKey.Length + 1];
+            tempDictValue = newDictValue;
+            newDictValue = new T2[tempDictValue.Length + 1];
+
+            for (int i = 0; i < tempDictKey.Length; i++)
+            {
+                newDictKey[i] = tempDictKey[i];
+            }
+            for (int i = 0; i < tempDictValue.Length; i++)
+            {
+                newDictValue[i] = tempDictValue[i];
+            }
+            newDictKey[newDictKey.Length - 1] = key;
+            newDictValue[newDictValue.Length - 1] = value;
+        }
+        public void PrintDict(int index)
+        {
+            Console.WriteLine(newDictKey[index - 1] + " : " + newDictValue[index - 1]);
+        }
+        public void PrintKeys()
+        {
+            foreach (var key in newDictKey)
+            {
+                Console.WriteLine(key);
+            }
+        }
+        public void PrintValues()
+        {
+            foreach (var value in newDictValue)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> newdict = new Dictionary<string, string>();//example of how dictionaries work and their features.
+            newdict.Add("City", "Şehir");
+            newdict.Add("Town", "Kasaba");
+            foreach (KeyValuePair<string, string> item in newdict)
+            {
+                Console.WriteLine(item.Key);
+            }
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>(); //example of using my method
+            myDictionary.Add("Key", "Anahtar");
+            myDictionary.Add("Value", "Değer");
+            myDictionary.Add("Item", "Esya");
+            myDictionary.Add("Dictionary", "Sozluk");
+            myDictionary.Add("Add", "Ekle");
+            myDictionary.Add("Main", "Ana");
+            myDictionary.PrintDict(1);
+            myDictionary.PrintDict(2);
+            myDictionary.PrintDict(3);
+            myDictionary.PrintDict(4);
+            myDictionary.PrintDict(5);
+            myDictionary.PrintDict(6);
+            myDictionary.PrintKeys();
+            myDictionary.PrintValues();
+        }
+    }
+}
